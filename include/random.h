2dfe2dde325d7577418f7c7b2cd27d8a21ac7eb1
Yazan <1210145@student.birzeit.edu>
2025-03-23 17:13:19 +0200

    Add header files for player, referee, and random modules

---
+++ include/random.h
@@ -0,0 +1,13 @@
+//
+// Created by - on 3/23/2025.
+//
+
+#ifndef RANDOM_H
+#define RANDOM_H
+
+#include <stdlib.h>
+#include <time.h>
+
+float random_float(float min, float max);
+
+#endif //RANDOM_H
