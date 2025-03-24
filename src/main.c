90b2c27aea68989973971610e218e47e8cd57ffc
Emil Khoury <khouryemil17@gmail.com>
2025-03-24 18:07:26 +0200

    Team Score

--- src/main.c
+++ src/main.c
@@ -11,7 +11,7 @@
 #define PATH_MAX 4096
 Config config;
 
-void fork_players(Team team, int num_players, char *bin_path);
+void fork_players(Team team, int num_players, char *bin_path,float energies[]);
 
 int main(int argc, char *argv[]) {
 
@@ -25,20 +25,45 @@ int main(int argc, char *argv[]) {
     load_config(config_path, &config);
     print_config(&config);
 
-    fork_players(TEAM_A, config.NUM_PLAYERS/2, bin_path);
-    fork_players(TEAM_B, config.NUM_PLAYERS/2, bin_path);
+    int num_players_per_team = config.NUM_PLAYERS / 2;
 
-    wait(NULL);
+    float energies_A[num_players_per_team];
+    float energies_B[num_players_per_team];
+
+    fork_players(TEAM_A, num_players_per_team, bin_path, energies_A);
+    fork_players(TEAM_B, num_players_per_team, bin_path, energies_B);
+
+    float total_energy_A = 0.0, total_energy_B = 0.0;
+
+    for (int i = 0; i < num_players_per_team; i++) {
+        total_energy_A += energies_A[i];
+        total_energy_B += energies_B[i];
+    }
+
+    int score = total_energy_A - total_energy_B;
+
+    printf("\nTotal Energy For Team A: %.2f\n", total_energy_A);
+    printf("Total Energy For Team B: %.2f\n", total_energy_B);
+    printf("Current Score: %d\n", score);
+
+    if (score > 0) {
+        printf("Team A is currently winning.\n");
+    } else if (score < 0) {
+        printf("Team B is currently winning.\n");
+    } else {
+        printf("It's a tie!\n");
+    }
 
     free(bin_path);
+    return 0;
 }
 
-void fork_players(Team team, int num_players, char *binary_path) {
+
+void fork_players(Team team, int num_players, char *binary_path, float energies[]) {
     Player players[num_players];
-    int pipefd[2];
-    
+
     for (int i = 0; i < num_players; i++) {
-        
+        int pipefd[2];
         if (pipe(pipefd) == -1) {
             perror("pipe");
             exit(EXIT_FAILURE);
@@ -77,11 +102,14 @@ void fork_players(Team team, int num_players, char *binary_path) {
             ssize_t bytes_read = read(pipefd[0], &energy, sizeof(float));
             if (bytes_read == sizeof(float)) {
                 printf("Received energy from player %d: %.2f\n", i, energy);
+                energies[i] = energy;
             } else {
                 printf("Failed to read energy from player %d\n", i);
+                energies[i] = 0.0;
             }
 
             close(pipefd[0]);
         }
     }
 }
+
