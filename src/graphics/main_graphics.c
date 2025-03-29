0fff0bd40ccde7d5725a524208bc789f5e1621cd
ahmadqaimari <ahmadqaimari@gmail.com>
2025-03-29 19:45:12 +0200

    Added OpenGL Test Code

--- src/graphics/main_graphics.c
+++ src/graphics/main_graphics.c
@@ -1,16 +1,38 @@
 #include <stdio.h>
+#include <sys/types.h>
+#include <unistd.h>
 #include <GL/glut.h>
-
-void display(void);
-
-int main(int argc, char **argv) {
-    glutInit(&argc, argv);
-    glutCreateWindow("Hello, OpenGL");
-    glutDisplayFunc(display);
-    glutMainLoop();
+void drawPlayer(float x, float y, float size) {
+    glBegin(GL_QUADS);
+    glVertex2f(x - size, y - size);
+    glVertex2f(x + size, y - size);
+    glVertex2f(x + size, y + size);
+    glVertex2f(x - size, y + size);
+    glEnd();
 }
 
-void display(void) {
+void drawRope(float x1, float y1, float x2, float y2) {
+    glBegin(GL_LINES);
+    glVertex2f(x1, y1);
+    glVertex2f(x2, y2);
+    glEnd();
+}
+
+void display() {
     glClear(GL_COLOR_BUFFER_BIT);
+    glColor3f(1.0, 0.0, 0.0);
+    drawPlayer(-0.5, 0.0, 0.1);
+    glColor3f(0.0, 0.0, 1.0);
+    drawPlayer(0.5, 0.0, 0.1);
+    glColor3f(0.0, 1.0, 0.0);
+    drawRope(-0.4, 0.0, 0.4, 0.0);
     glFlush();
+}
+int main(int argc, char** argv) {
+    glutInit(&argc, argv);
+    glutInitWindowSize(800, 600);
+    glutCreateWindow("Rope Pulling Game");
+    glutDisplayFunc(display);
+    glutMainLoop();
+    return 0;
 }
